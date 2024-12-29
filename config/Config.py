0ed4934073688f832f9a9ea52bbc0c68a1f780ee
nicolasances <nicolas.matteazzi@gmail.com>
2024-12-29 14:16:04 +0100

    Connection to Mongo

--- config/Config.py
+++ config/Config.py
@@ -1,3 +1,4 @@
+import string
 from totoapicontroller.model.TotoConfig import TotoConfig
 from totoapicontroller.model.singleton import singleton
 from totoapicontroller.model.TotoConfig import CloudProvider
@@ -5,10 +6,23 @@ from totoapicontroller.model.TotoConfig import CloudProvider
 @singleton
 class Config(TotoConfig): 
     
+    mongo_host: string
+    mongo_user: string 
+    mongo_pswd: string
+    
     def __init__(self):
         super().__init__(cloud_provider=CloudProvider.AWS)
+        
+        self.logger.log('INIT', 'Loading Mongo-related secretes')
+        
+        self.mongo_host = self.access_aws_secret_version(f"toto/{self.environment}/mongo-host", "eu-west-1")
+        self.mongo_user = self.access_aws_secret_version(f"toto/{self.environment}/toto-ms-tome-agent-mongo-user", "eu-west-1")
+        self.mongo_pswd = self.access_aws_secret_version(f"toto/{self.environment}/toto-ms-tome-agent-mongo-pswd", "eu-west-1")
         
         self.logger.log("INIT", "Configuration loaded!")
         
     def get_api_name(self) -> str:
         return "test-api"
+    
+    def get_mongo_connection_string(self): 
+        return f"mongodb://{self.mongo_user}:{self.mongo_pswd}@{self.mongo_host}:27017/tome"
--- dlg/test.py
+++ dlg/test.py
@@ -1,4 +1,4 @@
-
+from pymongo import MongoClient
 from flask import Request
 from agent.QuestionsGenerator import QuestionsGenerator
 from config.Config import Config
@@ -10,8 +10,30 @@ from totoapicontroller.model.ExecutionContext import ExecutionContext
 @toto_delegate(config_class=Config)
 def test(request: Request, user_context: UserContext, exec_context: ExecutionContext): 
     
-    exec_context.logger.log(exec_context.cid, f"It's working!")
+    config: Config = exec_context.config
     
-    resp = QuestionsGenerator(exec_context).generate_questions(num_questions=5)
+    client = None
+    
+    try: 
+        client = MongoClient(config.get_mongo_connection_string())
+        
+        db = client['tome']
+        
+        collection = db['quizQuestions']
+        
+        result = collection.insert_one({"quizId": '990d8a9s8d09a'})
+        
+        print(result)
+        
+    except Exception as e: 
+        print(f'ERROR: {e}')
+        return {}
+    
+    finally: 
+        if client: 
+            client.close()
+    
+    # resp = QuestionsGenerator(exec_context).generate_questions(num_questions=5)
+    resp = 'asd'
     
     return {"ok": True, "questions": resp}
