e08ba892ccbfd421501e6df191c211ba80ca151e
nicolasances <nicolas.matteazzi@gmail.com>
2024-12-29 15:56:46 +0100

    Added CORS headers

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from dlg.startQuiz import start_quiz
 from dlg.test import test
 
 app = Flask(__name__)
-CORS(app, origins=["*"])
+# CORS(app, origins=["*"])
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization", "toto-service", "x-correlation-id"]}})
 
 @app.route('/', methods=['GET'])
 def smoke():
