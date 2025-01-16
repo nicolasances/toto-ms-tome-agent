4ca26b726010144fc66b4ce925c6f8bc479aace5
nicolasances <nicolas.matteazzi@gmail.com>
2025-01-16 08:54:57 +0100

    Topic review picking

--- util/topicreview.py
+++ util/topicreview.py
@@ -1,26 +1,39 @@
 from random import random
 import re
+from pymongo import DESCENDING
 from pymongo.collection import Collection
 from model.topic import Topic
+from model.topicreview import TopicReview
 
-def find_next_topic(topics_collection: Collection) -> Topic:
+def find_next_topic(topics_collection: Collection, topic_reviews_coll: Collection) -> Topic:
     """Finds the next Topic to review. 
     
     Args:
         topics_collection (Collection): the collection where the topics are stored
+        topic_reviews_coll (Collection): the collection of topic reviews
         
     Returns:
         Topic: the next topic to review
     
     """
-    # Find the first topic found in the topics collection
+    
     topics = topics_collection.find().to_list()
+    trs = topic_reviews_coll.find().sort({"completedOn": DESCENDING})
     
-    topics_len = len(topics)
+    topic_codes = [Topic.from_bson(topic).code for topic in topics]
     
-    random_index = int(random() * topics_len)
-    topic_bson = topics[random_index]
+    # For each Topic Review, sorted in descending order of completion, the following loop will remove the corresponding 
+    # topic in the topics list
+    # The last topic standing is the oldest topic that has been reviewed, and should be the next in line.
+    for tr in trs: 
+        
+        topic_review = TopicReview.from_bson(tr)
+        
+        topics = [topic for topic in topics if Topic.from_bson(topic).code != topic_review.topic_code]
+        
+        # If there's only one topic left, return that one: it's the one reviewed the farthest
+        if len(topic_codes) == 1: 
+            return topics[0]
     
-    # Return a Topic object
-    return Topic.from_bson(topic_bson)
+    return {}
     
